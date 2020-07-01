053e0b022d2abb2432ad71276a35077c68689c5d
mgayle16 <64667298+mgayle16@users.noreply.github.com>
2020-07-01 14:10:38 -0400

    My Homework Submission

---
+++ VacationPY/api_keys.py
@@ -0,0 +1,6 @@
+# OpenWeatherMap API Key
+weather_api_key = "54db69e9487f4ef4406a25de7730ce1b"
+
+# Google API Key
+g_key = "AIzaSyB-0WNnnyNI9p1FWX5xZWDsN_Ciipv3qOU"
+
---
+++ WeatherPY/api_keys.py
@@ -0,0 +1,5 @@
+# OpenWeatherMap API Key
+weather_api_key = "54db69e9487f4ef4406a25de7730ce1b"
+
+# Google API Key
+g_key = "YAIzaSyBrkxKFD1on2N8ELnAiWu5EqV0pqSlMjn0"
